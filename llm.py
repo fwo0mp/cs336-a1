da6558745908c111a34785ed409123f2397df57c
Brendon Scheinman <bscheinman@gmail.com>
2025-06-26 12:46:55 -0400

    add rmsnorm

--- llm.py
+++ llm.py
@@ -38,3 +38,26 @@ class Embedding(torch.nn.Module):
             "... vocab_size, vocab_size embedding_dim -> ... embedding_dim"
         )
         return result
+
+
+class RMSNorm(torch.nn.Module):
+    def __init__(self, d_model: int, eps: float = 1e-5, device: Optional[torch.device] = None, dtype: Optional[torch.dtype] = None):
+        super().__init__()
+        self.d_model = d_model
+        self.eps = eps
+
+        self.weights = torch.nn.Parameter(torch.ones(self.d_model))
+
+    def forward(self, x: Float[torch.Tensor, "... d_model"]) -> Float[torch.Tensor, "... d_model"]:
+        original_dtype = x.dtype
+        x.to(torch.float32)
+
+        squared: Float[torch.Tensor, "... d_model"] = torch.pow(x, 2)
+        rms: Float[torch.Tensor, "... 1"] = torch.sqrt(self.eps + (squared.sum(dim=-1, keepdim=True)) / self.d_model)
+        weighted: Float[torch.Tensor, "... d_model"] = einsum(
+            x / rms,
+            self.weights,
+            "... d_model, d_model -> ... d_model"
+        )
+
+        return weighted.to(original_dtype)
--- tests/adapters.py
+++ tests/adapters.py
@@ -387,7 +387,12 @@ def run_rmsnorm(
         Float[Tensor,"... d_model"]: Tensor of with the same shape as `in_features` with the output of running
         RMSNorm of the `in_features`.
     """
-    raise NotImplementedError
+    from llm import RMSNorm
+
+    rmsnorm = RMSNorm(d_model=d_model, eps=eps)
+    rmsnorm.load_state_dict({ "weights": weights})
+
+    return rmsnorm(in_features)
 
 
 def run_silu(in_features: Float[Tensor, " ..."]) -> Float[Tensor, " ..."]:
